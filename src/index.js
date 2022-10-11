18136eeda9a400cae9bad897dd333128b2c938d1
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-11 08:36:24 +0200

    Újfajta függvényírás

---
+++ src/index.js
@@ -0,0 +1,29 @@
+function fvNeve(param1, param2){
+
+}
+
+let masikFv = function(param1, param2){
+    console.log(param1);
+}
+
+/*
+document.addEventListener('DOMContentLoaded',  function (){
+    console.log('Page Loaded');
+
+    document.getElementById('szinezes').addEventListener('click', function () {
+        document.body.style.backgroundColor = 'lightblue';
+    });
+});
+*/
+
+
+//  = 
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Page Loaded');
+
+    document.getElementById('szinezes').addEventListener('click', () => {
+        document.body.style.backgroundColor = 'lightblue';
+    });
+});
+
