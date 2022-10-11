9b5f81d4f679b9cef5a0b4de4682045a14fba6d3
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-11 09:03:42 +0200

    szinvaltas

--- src/index.js
+++ src/index.js
@@ -21,9 +21,23 @@ document.addEventListener('DOMContentLoaded',  function (){
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Page Loaded');
-
+    let idozito;
+    // Closure
     document.getElementById('szinezes').addEventListener('click', () => {
         document.body.style.backgroundColor = 'lightblue';
+        
+        setTimeout(() => {
+            document.body.style.backgroundColor = 'white';
+        }, 2000);
+        
+        if(idozito !== undefined){
+            console.log('Cleating timer', idozito)
+            clearInterval(idozito);
+        }
+        
+        idozito = setInterval(()  => {
+        console.log(document.body.style.backgroundColor);
+        }, 1000);
     });
 });
 
